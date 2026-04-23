92dd3a1510f42077fe6e712c4d42ec334c465d9e
Andrew <ajpahomi@cpp.edu>
2026-04-23 12:43:21 -0700

    Setting up files

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include "utils.h"
+
+ 
+
+int main() {
+
+    printf("Starting program...\n");
+
+    char *msg = get_message();
+
+    printf("Message: %s\n", msg);
+
+    process_data(NULL);
+
+    char *buffer = (char *)malloc(8);
+
+    strcpy(buffer, "This string is very long");
+
+    free(buffer);
+
+    buffer[0] = 'X';
+
+    char *message;
+
+    printf("Print Message: %s\n", message);
+
+    return 0;
+
+}
